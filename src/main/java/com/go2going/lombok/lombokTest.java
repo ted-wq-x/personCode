3e42ac212d1dd88520d1fa8175b207e55d94ff8d
wangq <736445126@qq.com>
2017-09-23 00:47:56 +0800

    add：初次提交

---
+++ src/main/java/com/go2going/lombok/lombokTest.java
@@ -0,0 +1,15 @@
+package com.go2going.lombok;
+
+/**
+ * 项目名称：  testcode
+ * 类名称：  lombokTest
+ * 描述：
+ *
+ * @author wangqiang
+ * 创建时间：  2017/9/22 0022 15:49
+ */
+public class lombokTest {
+
+
+
+}
