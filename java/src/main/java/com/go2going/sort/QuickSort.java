eebf0306a8b769002da1ed55883efd66a00d0e33
wangq <736445126@qq.com>
2017-12-07 12:44:36 +0800

    modify:quick sort

--- java/src/main/java/com/go2going/sort/QuickSort.java
+++ java/src/main/java/com/go2going/sort/QuickSort.java
@@ -5,63 +5,69 @@ import java.util.Arrays;
 /**
  * 项目名称：  testcode<br>
  * 类名称：  QuickSort<br>
- * 描述：
+ * 描述：快速排序：
  *
  * @author wangqiang
  * 创建时间：  2017/11/7 0007 18:15
  */
 public class QuickSort {
 
-//    TODO
-static int[] arr;
 
+    /**
+     * 找出基准，将比基准小的放到左边，大的放到右边，递归排序左边和右边
+     *
+     * @param arr
+     */
     public static void quickSort(int[] arr) {
-        QuickSort.arr = arr;
-        sort( 0, arr.length-1);
-
-    }
-
-    public static void sort(int start, int end) {
-        if (start >= end) {
+        if (arr == null || arr.length == 0) {
             return;
         }
+        sort(arr, 0, arr.length - 1);
 
-        int index = partition(start, end);
-        if (index - 1 > start) {
-            sort(start, index - 1);
-        }
-        if (index + 1 < end) {
-            sort(index+1, end);
-        }
     }
 
+    public static void sort(int[] arr, int start, int end) {
+        //将会第一个数作为中立元素
 
-    public  static int partition(int start,int end) {
+        int mid = arr[(start + end) / 2];
 
-        int mid = arr[start];
+        //双指针逼近
+        int i = start, j = end;
 
-        int i = start + 1,j=end;
-
-        while (i < j) {
-            while (arr[i] <= mid&&i<end) {
+        while (i <= j) {
+            //从左往右找比mid大的。从右往左找比mid小的，然后交换，相等的也要交换
+            while (arr[i] < mid) {
                 i++;
             }
-            while (arr[j] >= mid&&j>start) {
+            while (arr[j] > mid) {
                 j--;
             }
-            swap(i, j);
-
+            if (i <= j) {
+                //交换过的就不用比较了
+                swap(arr, i++, j--);
+            }
         }
-        swap(start, i);
-        return j;//j==i
+
+        //递归调用左右两边的组数
+        if (j > start) {
+            sort(arr, start, j);
+        }
+        if (i < end) {
+            sort(arr, i, end);
+        }
     }
-    private static void swap(int x, int y) {
+
+    private static void swap(int[] arr, int x, int y) {
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
     }
+
     public static void main(String[] args) {
-        int[] a = {12, 33, 54, 23, 58, 5, 5,888};
+        //                int[] a = {10,2,3,4,5};
+        //        int[] a = {12, 33, 54, 23, 58, 5, 5, 888};
+        int[] a = {1, 33, 2, 23, 58, 5, 5, 2};
+
         quickSort(a);
         System.out.println(Arrays.toString(a));
     }
