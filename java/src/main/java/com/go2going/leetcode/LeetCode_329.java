2031c7dcbe95cbab635020ea3d5e0b4479253808
wangq <736445126@qq.com>
2018-05-26 19:41:20 +0800

    add:leetCode_827

--- java/src/main/java/com/go2going/leetcode/LeetCode_329.java
+++ java/src/main/java/com/go2going/leetcode/LeetCode_329.java
@@ -1,6 +1,6 @@
 package com.go2going.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * <a href="https://www.youtube.com/watch?v=yKr4iyQnBpY">参考<a/>
@@ -20,12 +20,13 @@ public class LeetCode_329 {
     /**
      * time,space:mn
      * DFS 16ms
+     *
      * @param matrix
      * @return
      */
     public int longestIncreasingPath(int[][] matrix) {
 
-        if (matrix == null||matrix.length==0) {
+        if (matrix == null || matrix.length == 0) {
             return 0;
         }
         int length = matrix.length;
@@ -51,6 +52,7 @@ public class LeetCode_329 {
 
     /**
      * dfs查找最大值
+     *
      * @param i
      * @param j
      * @return
@@ -78,13 +80,73 @@ public class LeetCode_329 {
             }
 
             //当前长度1+四个位置的dfs值
-            dp[i][j] = Math.max(dp[i][j], 1+dfs(tx, ty));
+            dp[i][j] = Math.max(dp[i][j], 1 + dfs(tx, ty));
 
         }
 
 
-
         return dp[i][j];
+    }
+
+    /**
+     * 方法二 bottom-up 有问题
+     *
+     * @param matrix
+     * @return
+     */
+    public int longestIncreasingPath1(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int length = matrix.length;
+        int height = matrix[0].length;
+
+        int[][] dp = new int[length][height];
+        for (int i = 0; i < length; i++) {
+            //此处初初始化值为1
+            Arrays.fill(dp[i], 1);
+        }
+
+        int max = 0;
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < height; j++) {
+                list.add(new int[]{matrix[i][j], i, j});
+            }
+        }
+
+        //逆序排序,该成从小到大是对的
+        list.sort((o1, o2) -> o2[0] - o1[0]);
+
+        int[] temp = {-1, 1, 0, 0};
+
+        for (int[] ints : list) {
+            int x = ints[1];
+            int y = ints[2];
+            for (int i = 0; i < 4; i++) {
+                int tx = x + temp[i];
+                int ty = y + temp[3 - i];
+                if (tx < 0 || ty < 0 || tx >= length || ty >= height) {
+                    continue;
+                }
+
+                if (matrix[x][y] <= matrix[tx][ty]) {
+                    continue;
+                }
+                dp[x][y] = Math.max(dp[x][y], 1 + dp[tx][ty]);
+            }
+            max = Math.max(max, dp[x][y]);
+        }
+
+
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode_329 leetCode_329 = new LeetCode_329();
+        System.out.println(leetCode_329.longestIncreasingPath1(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
+
     }
 
 }
