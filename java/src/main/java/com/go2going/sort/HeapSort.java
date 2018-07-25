28dda62fe13e9ac9d68e5084acf2b8f86e8c519e
ted-wq-x <736445126@qq.com>
2018-07-25 21:02:07 +0800

    add: :fireworks:leetcode_215

--- java/src/main/java/com/go2going/sort/HeapSort.java
+++ java/src/main/java/com/go2going/sort/HeapSort.java
@@ -21,56 +21,6 @@ public class HeapSort {
     }
 
     /**
-     * 堆排序的主要入口方法，共两步。
-     */
-    public void sort() {
-
-        //1.创建最大堆
-        int length = arr.length - 1;
-        int beginIndex = (length - 1) >> 1;
-        for (int i = beginIndex; i >= 0; i--) {
-            heapMax(i, length);
-        }
-
-        //2.找到最大值，方队数组的最后，那么下次循环的时候就不用排序这个了
-        for (int i = length; i > 0; i--) {
-            swap(0, i);
-            heapMax(0, i - 1);//i-1的意思得明白
-        }
-    }
-
-
-    public void heapMax(int i, int length) {
-        int left = (i << 1) + 1;//左节点
-        int right = left + 1;//右节点
-
-        //注意边界的判断，左边界直接返回
-        if (left > length) {
-            return;
-        }
-
-        //两个子节点的最大值和父节点比较
-        int max = left;
-
-        //有边界越界时，只需要比较左边界就行
-        if (right <= length && arr[left] < arr[right]) {
-            max = right;
-        }
-
-        if (arr[max] > arr[i]) {
-            swap(max, i);
-            heapMax(max, length);
-        }
-    }
-
-    public void swap(int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-
-
-    /**
      * 测试用例
      * <p>
      * 输出：
@@ -80,6 +30,62 @@ public class HeapSort {
         int[] arr = new int[]{3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6};
         new HeapSort(arr).sort();
         System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 堆排序的主要入口方法，共两步。
+     */
+    public void sort() {
+
+        //1.创建最大堆
+        int length = arr.length;
+        int beginIndex = (length - 1) >> 1;
+        //i都是有子节点的根节点
+        for (int i = beginIndex; i >= 0; i--) {
+            heapMax(i, length);
+        }
+
+        //构建玩最大堆之后，还不能保证左右节点的大小，只知道根节点最大
+
+        //2.找到最大值，方队数组的最后，那么下次循环的时候就不用排序这个了
+        for (int i = length-1; i > 0; i--) {
+            swap(0, i);
+            //将最大值放到最后，然后重新构建
+            heapMax(0, i );
+        }
+    }
+
+    /**
+     * left和right+1的原因是在计算beginIndex的位置时就少了1，因为下标是从0开始的
+     *
+     * @param i
+     * @param length
+     */
+    public void heapMax(int i, int length) {
+        int left = (i << 1) + 1;//左节点
+        int right = left + 1;//右节点
+        int maxIndex = i;
+
+        if (left < length && arr[left] > arr[maxIndex]) {
+            maxIndex = left;
+        }
+
+        if (right < length && arr[right] > arr[maxIndex]) {
+            maxIndex = right;
+        }
+
+        if (maxIndex != i) {
+            swap(maxIndex, i);
+            //递归调整
+            heapMax(maxIndex, length);
+        }
+
+    }
+
+    public void swap(int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
 }
